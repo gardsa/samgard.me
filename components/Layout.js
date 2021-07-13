db49edba48381d155506872dd04aaa3db0a3ddfc
Samantha Gard <sam.gard@abletech.co.nz>
2021-07-13 19:29:56 +1200

    feat: coming soon page

---
+++ components/Layout.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+
+const name = 'Sam Gard'
+const role = 'Front-End Developer'
+export const siteTitle = 'Sam Gard - Front-End Developer'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content={siteTitle} />
+        <meta property='og:image' content='https://www.samgard.me/images/profile.jpg' />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src='/images/profile.jpg'
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <h2 className={`${utilStyles.headingLg} ${utilStyles.headingOrange}`}>
+              {role}
+            </h2>
+          </>
+        ) : (
+          <>
+            <Link href='/'>
+              <a>
+                <Image
+                  priority
+                  src='/images/profile.jpg'
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href='/'>
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href='/'>
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.centerText}>
+        <p className={utilStyles.headingMd}>Coming soon!</p>
+        <p>
+          In the meantime, feel free to contact me at{' '}
+          <a href='mailto:hello@samgard.me'>hello@samgard.me</a>
+        </p>
+      </section>
+    </Layout>
+  )
+}
